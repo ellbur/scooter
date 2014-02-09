d23434c81c81d4d78b38cbe63a49d47273f0d1c4
Owen Healy <owen@pulsetor.com>
2014-02-08 23:39:15 -0500

    Passing extra objects to gcc linker

--- scooter/gcc.py
+++ scooter/gcc.py
@@ -7,14 +7,14 @@ def compile_gcc(build, src, obj, opts, cc='gcc'):
 def link_gcc(build, objs, bin, opts, ld='gcc'):
     return build.watching(set(_.dir for _ in objs)).easyrun(ld, '-o', bin, opts, objs)
 
-def build_gcc(build, sources, bin, opts, gcc='gcc'):
+def build_gcc(build, sources, bin, opts, gcc='gcc', extra_objects=()):
     objects = [ ]
     for s in sources:
         obj = build.mkobj(s, '.o')
         objects.append(obj)
         compile_gcc(build, s, obj, opts, cc=gcc)
             
-    link_gcc(build, objects, bin, opts, ld=gcc)
+    link_gcc(build, objects + list(extra_objects), bin, opts, ld=gcc)
 
 def run_gcc(build, sources, opts, gcc='gcc', into=None):
     bin = build.mkobj(sources, '')
