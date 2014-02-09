8b3cedb3e73c344f1922a080cf229daf8c1f2b04
Owen Healy <owen@pulsetor.com>
2014-02-08 23:38:45 -0500

    Move to quickfiles and easyrun

--- scooter/scooter.py
+++ scooter/scooter.py
@@ -14,6 +14,8 @@ import time
 import hashlib
 import types
 from quickstructures import *
+from quickfiles import *
+from easyrun import *
 
 # http://stackoverflow.com/questions/1151658/python-hashable-dicts
 class hashable_dict(dict):
@@ -52,146 +54,12 @@ def augment(the_obj, the_name, the_field):
                 return setattr(the_obj, name, next)
     return Augmented()
     
-class Path(nt('Path', ['path'])):
-    @staticmethod
-    def mktemp():
-        import tempfile
-        _, path = tempfile.mkstemp()
-        return p(path)
-    def __repr__(self): return './' + self.path
-    @property
-    def realpath(self): return os.path.realpath(self.path)
-    def __truediv__(self, next):
-        return self.__div__(next)
-    def __div__(self, next):
-        return p(str(self) + '/' + str(next))
-    @property
-    def exists(self): return os.path.exists(str(self))
-    def against(self, where): return './' + os.path.relpath(str(self), str(where))
-    def replant(self, src, dst): return p(dst) / self.against(src)
-    def indir(self, dst): return self.replant(self.dir, dst)
-    @property
-    def isdir(self): return os.path.isdir(str(self))
-    def chext(self, old, new):
-        assert self.path.endswith(old)
-        return p(seld.path[:-len(old)] + new)
-    def setext(self, new):
-        prefix = re.sub(r'\.[^\.\/]*$', '', self.path)
-        return p(prefix + new)
-    @property
-    def name(self): return os.path.split(os.path.realpath(str(self)))[1]
-    @property
-    def dir(self):
-        if os.path.isdir(str(self)): return self
-        else: return p(os.path.split(os.path.realpath(str(self)))[0])
-    def make_parents(self): self.dir.makedir()
-    def mkdir(self): return self.makedir()
-    def makedir(self):
-        try:
-            os.makedirs(str(self))
-        except OSError as e:
-            if e.errno != 17: raise e
-    @property
-    def name(self):
-        return os.path.split(os.path.realpath(str(self)))[1]
-    @property
-    def realpath(self):
-        return os.path.realpath(str(self))
-    def __floordiv__(self, pat):
-        if pat == '**':
-            dirs = []
-            def see(_, dir, __):
-                dirs.append(p(dir))
-            os.path.walk(str(self), see, None)
-            return PTuple(dirs)
-        else:
-            return PTuple(p(_) for _ in glob(str(self) + '/' + pat))
-    def __mod__(self, how):
-        if isinstance(how, RAND):
-            import tempfile
-            self.makedir()
-            _, path = tempfile.mkstemp(dir=str(self), prefix=how.prefix, suffix=how.suffix)
-            return p(path)
-        elif isinstance(how, COUNTER):
-            candidates = (_ for _ in os.listdir(str(self)) if _.startswith(how.prefix))
-            number_strs = (c[len(how.prefix):] for c in candidates)
-            numbers = [ ]
-            for s in number_strs:
-                try: numbers.append(int(s))
-                except ValueError: pass
-            now = max(numbers) + 1 if len(numbers)>0 else 1
-            return self/(how.prefix + str(now))
-        else:
-            raise TypeError(how, 'should be one of', [TMP, COUNTER])
-    
-    def rm(self): os.unlink(str(self))
-    def set(self, wth):
-        self.make_parents()
-        open(str(self), 'w').write(wth)
-    def clear(self):
-        self.set('')
-    def setas(self, wth):
-        self.make_parents()
-        wth(open(str(self), 'w'))
-    def append(self, wth):
-        self.make_parents()
-        open(str(self), 'a').write(wth)
-    def open(self):
-        return open(str(self), 'r')
-    def cp(self, dest):
-        dest = p(dest)
-        if dest.isdir:
-            dest = dest / self.name
-        if self.isdir:
-            shutil.copytree(str(self), str(dest))
-        else:
-            shutil.copy2(str(self), str(dest))
-    
-class COUNTER(nt('COUNTER', ['prefix'])): pass
-_RAND = nt('RAND', ['prefix', 'suffix'])
-class RAND(_RAND):
-    @staticmethod
-    def __new__(_cls, prefix='', suffix=''):
-        return _RAND.__new__(_cls, prefix, suffix)
-
-def p(s):
-    if isinstance(s, Path): return s
-    elif isinstance(s, basestring): return Path(os.path.normpath(os.path.relpath(s)))
-    else: raise TypeError
-_p = p
-
 class BuildFailed(Exception):
     def __init__(self, msg):
         Exception.__init__(self)
         self.msg = msg
     def __repr__(self): return self.msg
     def __str__(self): return self.msg
-
-def abbrev(cmd):
-    return p(cmd[0]).name + ' ... ' + ' '.join(p(_).name
-        for _ in cmd[1:] if not _.startswith('-'))
-
-class Wild: pass
-
-def clean(cmd, wd=None, first=True):
-    cleaned = []
-    for c in cmd:
-        if isinstance(c, Path):
-            c.make_parents()
-        
-        if isinstance(c, Path):
-            if wd != None:
-                cleaned.append(c.against(wd))
-            else:
-                cleaned.append(str(c))
-        elif isinstance(c, list) or isinstance(c, tuple):
-            cleaned.extend(clean(c, wd=wd, first=first))
-        else:
-            cleaned.append(str(c))
-        
-        first = False
-        
-    return t(cleaned)
 
 class Command(nt('Command', ['func', 'args', 'kwargs'])):
     def __call__(self):
@@ -201,10 +69,6 @@ class Command(nt('Command', ['func', 'args', 'kwargs'])):
     def is_up2date(self):
         return True
     
-def easyrun(*cmd, **kwargs):
-    wd = kwargs.get('wd', None)
-    return run(clean(cmd, wd), **kwargs)
-
 def spy(hl, into=sys.stdout):
     from fcntl import fcntl, F_GETFL, F_SETFL
     from select import select
@@ -401,9 +265,6 @@ def do_build(op):
 
 build = do_build
     
-t = tuple
-def tt(*things): return tuple(things)
-
 def on_new_thread(func):
     from threading import Thread
     th = Thread(None, func)
