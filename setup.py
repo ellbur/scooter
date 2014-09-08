7c78e839fcd20a0549111e285c58e402e7a46a97
Owen Healy <owen@pulsetor.com>
2014-09-07 23:16:25 -0400

    Better installer

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name       = 'scooter',
-    version    = '0.123',
+    version    = '2',
     packages   = ['scooter'],
-    py_modules = ['scooter.gcc', 'scooter.llvm']
+    py_modules = ['scooter.gcc', 'scooter.llvm'],
+    # http://stackoverflow.com/questions/12372336/how-do-i-make-pip-respect-requirements
+    install_requires = [
+        'quickfiles',
+        'quickstructures',
+        'easyrun',
+    ],
+    # http://pythonhosted.org/setuptools/setuptools.html#dependencies-that-aren-t-in-pypi
+    dependency_links = [
+        'https://github.com/ellbur/quickfiles/archive/master.zip#quickfiles-0.123',
+        'https://github.com/ellbur/quickstructures/archive/master.zip#quickstructures-0.123',
+        'https://github.com/ellbur/easyrun/archive/master.zip#easyrun-0.123',
+    ]
 )
 
