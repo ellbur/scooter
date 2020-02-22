9b4dc0f053408c9a356cf9cb9a6e186c1d99d019
Owen Healy <owen@pulsetor.com>
2020-02-22 15:47:56 -0500

    Stick with pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name       = 'scooter',
-    version    = '2.2',
+    version    = '2.3',
     packages   = ['scooter'],
     py_modules = ['scooter.gcc', 'scooter.llvm'],
     # http://stackoverflow.com/questions/12372336/how-do-i-make-pip-respect-requirements
@@ -12,12 +12,6 @@ setup(
         'quickfiles',
         'quickstructures',
         'ellbur-easyrun',
-    ],
-    # http://pythonhosted.org/setuptools/setuptools.html#dependencies-that-aren-t-in-pypi
-    dependency_links = [
-        'https://github.com/ellbur/quickfiles/archive/master.zip#quickfiles-0.123',
-        'https://github.com/ellbur/quickstructures/archive/master.zip#quickstructures-0.123',
-        'https://github.com/ellbur/easyrun/archive/master.zip#easyrun-0.123',
     ]
 )
 
